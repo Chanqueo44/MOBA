794f9225e9e5bb4161273c55c0d7adfaf6d3c407
Chanqueo44 <sofiachanqueo2002@gmail.com>
2022-12-15 05:49:23 -0300

    added Daisy W for Q demonstration

---
+++ My project/Assets/MobaGame/ImplementedHeroes/DaisyW/DaisyWalker.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DaisyWalker : Hero
+{
+     public override void giveRewards(){
+
+    }
+    protected override void basicAttack(){
+        
+    }
+
+    public override void useAbility1(){
+    }
+    
+    public override void useAbility2(){
+        
+    }
+    public override void useAbility3(){
+        
+    }
+    public override void useAbility4(){
+        
+    }
+}
